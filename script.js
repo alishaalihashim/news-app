1d7a24a876af6c82214ab0fb6d76dcfb6866e2ce
VB Student <vbstudent@uit.edu>
2018-09-22 14:41:22 +0500

    my work

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-$.ajax({
-	url: "https://newsapi.org/v2/top-headlines?sources=football-italia&apiKey=14a531983ea6412da67cf3e9d803c5a0",
-	success: function(data){
+function initial(){
 
-		
-		for(let i =1; i<=9; i++){
+	$.ajax({
+		url:"https://newsapi.org/v2/top-headlines?sources=ary-news&apiKey=6014b9f1114b426c98f2db15ff60e190",
+		success: function(data){
+			console.log(data);
+			for(i=0; i<7; i++){
+				document.querySelector(".main-container").innerHTML +=
+				`<div>
+				<h1> ${data.articles[i].title}</h1>
+				<p> ${data.articles[i].description}</p>
+				</div>
+				<div class="head-line">
+				<span class="news-image" style="background-image:url(${data.articles[i].urlToImage});"><span>
+				<div class="border-set"></div>
 
-			document.querySelector(".main").innerHTML+=`
-			<div class="card" id="news">
-			<div class="card-img-top img" style="background-image: url(${data.articles[i].urlToImage})"></div>
-			<div class="card-body">
-			<h3 class="card-title author">${data.articles[i].author}</h3>
-			
-			<a href="${data.articles[i].url}" target="_blank"> <p class="card-text des">${data.articles[i].title}</p></a>
-			<p class="para">It's all about Sports</p>		
-			</div>
-			</div>
+				</div>`
 
-			`
+			}
 		}
-		
-		
 
-	},
-
-	error: function(error){
-		console.log(error);
-	}
-
-})
-
+	})
+}
 
